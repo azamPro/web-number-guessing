165b32d8b75bc3870b92573bf3db512abbc56f9f
azam <azam.alkhodiriy@gmail.com>
2023-11-19 13:32:15 +0300

    finish the game

--- main.js
+++ main.js
@@ -18,9 +18,17 @@ function startGame() {
     document.getElementById('range').style.display = 'block'
 }
 //set range function
-function setRange() {
-    min= document.getElementById('min').value
-    max= document.getElementById('max').value
+function setRange(minn,maxx) {
+    
+    // if min and max not empty assign the value to the variable
+    if(document.getElementById('min').value == '' && document.getElementById('max').value == '') {
+        min = Number(minn)
+        max = Number(maxx)
+        //startGuess()
+    }else{
+        min = parseInt(document.getElementById('min').value);
+        max = parseInt(document.getElementById('max').value);
+    }
     if(check()) return;
     startGuess()
 }
@@ -40,19 +48,27 @@ function check(){
 function startGuess() {
     document.getElementById('range').style.display = 'none'
     document.getElementById('start-the-game').style.display = 'block'
+    document.getElementById('result').style.display = 'inline-block'
+    document.getElementById('guess').style.display = 'inline-block' 
+    document.getElementById('title-again').style.display = 'block';
+    document.getElementById('guess-button').style.display = 'inline-block';
+    document.getElementById('result').style.display = 'none'
+
     //set the random number between the range min and max
-    randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
-    console.log(randomNumber)
+    randomNumber = Math.floor(min + Math.random() * (max - min + 1));
 }
 
 //check if the number is correct
 function isCorrect() {
     guessNumber = document.getElementById('guess').value
+    
     if(guessNumber == randomNumber) {
         alert('you win!! congration')
-        document.getElementById('result').style.display = 'none'
-        document.getElementById('guess').style.display = 'none'
-        document.getElementById('again').style.display = 'block'
+        document.getElementById('title-again').innerHTML = 'start again?';
+        document.getElementById('guess').style.display = 'none';
+        document.getElementById('guess-button').style.display = 'none';
+        document.getElementById(`result`).style.display = 'none';
+        document.getElementById('again').style.display = 'inline-block';
         
     } else if(guessNumber < randomNumber) {
         document.getElementById('result').style.display = 'block'
@@ -68,10 +84,14 @@ function reset() {
     max= ''
     randomNumber = ''
     guessNumber = ''
-
+    // reset all style and value
     document.getElementById('guess').value = ''
     document.getElementById('result').style.display = 'none'
-    document.getElementById('guess').style.display = 'block'
+    document.getElementById('guess').style.display = 'none'
     document.getElementById('again').style.display = 'none'
-    startGuess()
+    document.getElementById('start-the-game').style.display = 'none'
+    document.getElementById('again').style.display = 'none';
+    document.getElementById('title-again').style.display = 'none';
+    document.getElementById('start').style.display = 'block'
+    start()
 }
